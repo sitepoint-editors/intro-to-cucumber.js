6a649760e258530759d848e4c6adc130707e4496
Graham Cox <graham@grahamcox.co.uk>
2017-05-19 22:27:50 +0100

    Simple example

---
+++ simple/steps/maths.js
@@ -0,0 +1,24 @@
+const defineSupportCode = require('cucumber').defineSupportCode;
+const assert = require('assert');
+const expect = require('chai').expect;
+
+defineSupportCode(function({ Given, Then, When }) {
+    let answer = 0;
+
+    Given('I start with {int}', function (input) {
+        answer = input;
+    });
+
+    When('I add {int}', function (input) {
+        answer = answer + input;
+    });
+	When('I multiply by {int}', function (input) {
+		answer = answer * input;
+	});
+
+    Then('I end up with {int}', function (input) {
+        expect(answer).to.equal(input);
+    });
+
+});
+

1628328948b58206356ea81deceda1ff533bd473
Graham Cox <graham@grahamcox.co.uk>
2017-05-19 22:31:59 +0100

    Support for running simple example with NPM, Grunt and Gulp

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+
+// Project configuration.
+grunt.initConfig({
+	shell: {
+		simple: {
+            command: 'cucumber.js simple/features -r simple/steps'
+		}
+	}
+});
+
+// These plugins provide necessary tasks.
+grunt.loadNpmTasks('grunt-shell');
+};
